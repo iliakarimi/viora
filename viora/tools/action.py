3d2d3f2e1e703470633dffb53438f75860cc88eb
iliakarimi <theiliakarimi@gmail.com>
2026-06-10 18:05:46 +0330

    Action Tools Development Complete

--- viora/tools/action.py
+++ viora/tools/action.py
@@ -1,55 +1,62 @@
-import Xlib
-import pyautogui
-import subprocess
-from time import sleep
-from utils.encode import encode_image
+# import Xlib
+import pyautogui as pg
 
 
 
 class ComputerControl():
+    
     """
     Control Computer
     """
 
-    def __init__(self, key= '', times=1, write= '', firsthkey= '', sechkey= '', hotkey= '', left_click=None, right_click=None, wait_time=None, scroll=None): #The 'None' for mouse Parameter's temperary.
-        self.key = key
-        self.times = times
-        self.write = write
-        self.firsthkey = firsthkey
-        self.sechkey = sechkey
-        self.hotkey = hotkey
-        self.left_click = left_click
-        self.right_click = right_click
-        self.wait_time = wait_time
-        self.scroll = scroll
-
-    def keyboard_control(self, key=None, times=None, write=None, firsthkey=None, sechkey=None, hotkey=None):
+    @classmethod
+    def keyboard_control(
+                cls, key= '', key_times=1, write= '',
+                firsthkey= '', sechkey= '', hotkey= '',
+            ):
 
         if write:
-            pyautogui.write(write)
+            pg.write(write)
             return
 
         if key:
-            pyautogui.press(key, presses=int(times) if times else 1)
+            pg.press(key, presses=int(key_times) if key_times else 0)
             return
 
         if hotkey:
             keys = hotkey.split('+')
-            pyautogui.hotkey(*keys)
+            pg.hotkey(*keys)
             return
 
         if firsthkey and sechkey:
-            with pyautogui.hold(firsthkey):
-                pyautogui.press(sechkey)
+            with pg.hold(firsthkey):
+                pg.press(sechkey)
             return
 
-    def mouse_control(self, left_click, right_click, wait_time, scroll, move):
-        pyautogui.leftClick()
-        if right_click:
-            right_click = pyautogui.click(right_click)
-            for rc in right_click:
-                sleep(wait_time)
-                return rc 
+
+    @classmethod
+    def mouse_control(
+                cls, movex=None, movey=None, 
+                click_button=None, click_times=0, scroll=0
+            ):
         
-        m_scrool = pyautogui.scroll(scroll)
-        pass
+        if movex or movey is not None or 0:
+            try:
+                pg.moveTo(x=movex, y=movey)
+                
+            except Exception as e:
+                return f"Error: {e}"
+            return
+        
+        elif click_times or click_button:
+            try:
+                pg.click(clicks=click_times, button=click_button)
+            except Exception as e:
+                return f"Error: {e}"
+            return
+        
+        elif scroll:
+           try:
+            pg.vscroll(clicks=scroll)
+           except Exception as e:
+               return f"Error: {e}"
