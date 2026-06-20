e5a56db0c338141783ddcb3228bf9788d35bc9d3
iliakarimi <theiliakarimi@gmail.com>
2026-06-20 22:11:16 +0330

    Change Screen_picture Function Description

--- viora/utils/snapshot.py
+++ viora/utils/snapshot.py
@@ -1,18 +1,17 @@
+import Xlib
 import pyautogui
 import subprocess
-import Xlib
+
 
 def __screen_picture():
     """
-    Take an ScreenShot from the Screen
+    This function just Take an ScreenShot from the Screen
     """
 
     try:
         pyautogui.screenshot('logs/snapshot.png')
-    
     except Xlib.error.DisplayConnectionError:
         subprocess.call(["xhost", "+"])
         pyautogui.screenshot('logs/snapshot.png')
-    
     except Exception as e:
-        raise f"An Error Happend: {e}"
+        raise RuntimeError(f"An Error Happend: {e}")
