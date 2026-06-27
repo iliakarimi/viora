58bf521993bce1f92c2c7324111c20e3600cdf3f
iliakarimi <theiliakarimi@gmail.com>
2026-06-27 16:45:10 +0330

    Write A Temporary Memory

--- viora/memory/__init__.py
+++ viora/memory/__init__.py
@@ -1,1 +1,1 @@
-from short_term_memory import ShortTermMemory
+from memory.short_term_memory import ShortTermMemory
--- viora/memory/short_term_memory.py
+++ viora/memory/short_term_memory.py
@@ -1,12 +1,14 @@
 class ShortTermMemory:
-    def __init__(self):
+    def __init__(self, max_turns=5):
+        self.max_turns=max_turns
         self.messages = []
-
+    
     def add_message(self, role, content):
         self.messages.append({"role": role, "content": f"{content}"})
 
     def get_messages(self):
-        return self.messages
+        return self.messages[-(self.max_turns * 2):]
 
     def clear(self):
         self.messages = []
+
