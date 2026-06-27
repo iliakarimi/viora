8c9da95c5b35242a76431cfe0191380ae821c636
iliakarimi <theiliakarimi@gmail.com>
2026-06-27 16:46:08 +0330

    Complete TTS File

--- viora/speech/tts.py
+++ viora/speech/tts.py
@@ -1,54 +1,33 @@
 import os
 import json
-import pygame
+import soundfile as sf
+import sounddevice as sd
 from dotenv import load_dotenv
 from deepgram import DeepgramClient
-
-
-
-with open("configs/tts_setting.json", "r") as r:
-    tts_configs = json.load(r)
 
 
 load_dotenv()
 
 
+with open("configs/tts_setting.json", "r") as r:
+    tts_configs = json.load(r)
 
-TTSvolume = tts_configs["tts_volume"]
 voice_select = tts_configs["voice_select"]
 Sample_Rate = tts_configs["sample_rate"]
 encoding = tts_configs["encoding"]
 
 
 
-pygame.mixer.init()
-class TTSPlayer:
-    def __init__(self):
-        self.current_sound = None
+def ttsplayer(file_path):
+    """ Play Audio file """
+    data, fs = sf.read(file_path, dtype='float32')  
+    sd.play(data, fs)
+    status = sd.wait()  # Wait until file is done playing
 
 
-    def play(self, file_path):
-        """ Play Audio file """
-        if self.current_sound:
-            self.stop()
-        
-        self.current_sound = pygame.mixer.Sound(file_path)
-        self.current_sound.set_volume(TTSvolume)
-        self.current_sound.play()
-
-
-    def stop(self):
-        """ Stop Audio file"""
-        if self.current_sound:
-            self.current_sound.stop()
-            self.current_sound = None
-tts_player = TTSPlayer()
-
-
-
-def modeltts(respond):
+def main_tts(text):
     
-    SPEAK_TEXT = respond
+    SPEAK_TEXT = text
     filename = "logs/response.mp3"
 
     try:
@@ -67,9 +46,7 @@ def modeltts(respond):
             # Iterate through the generator and write chunks
             for chunk in response:
                 audio_file.write(chunk)
-        tts_player.play(filename)
-        while pygame.mixer.get_busy():
-            pygame.time.delay(100)
-        
+        ttsplayer(filename)
+
     except Exception as e:
         print(f"Exception: {e}")
