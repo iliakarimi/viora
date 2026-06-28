6e2a9ca08bf6179399274b874b5f4ed4bc925c9c
iliakarimi <theiliakarimi@gmail.com>
2026-06-28 18:26:01 +0330

    Almost Finishing for the First Release

---
+++ viora/chat.py
@@ -0,0 +1,19 @@
+import asyncio
+from speech.tts import main_tts
+from memory import ShortTermMemory
+from tools import ComputerControl as cc
+
+
+short_term_memory = ShortTermMemory()
+
+
+async def run_agent():
+    asyncio.get_running_loop()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- viora/memory/short_term_memory.py
+++ viora/memory/short_term_memory.py
@@ -1,14 +1,18 @@
 class ShortTermMemory:
     def __init__(self, max_turns=5):
         self.max_turns=max_turns
-        self.messages = []
+        self.agent_messages = []
+        self.user_messages = []
     
-    def add_message(self, role, content):
-        self.messages.append({"role": role, "content": f"{content}"})
+    def add_message(self, content):
+        self.user_messages.append({"role": "user", "content": f"{content}"})
+        self.agent_messages.append({"role": "assistant", "content": f"{content}"})
 
-    def get_messages(self):
-        return self.messages[-(self.max_turns * 2):]
+    def get_user_messages(self):
+        return self.user_messages[-(self.max_turns * 2):]
+    
+    def get_agent_messages(self):
+        return self.agent_messages[-(self.max_turns * 2):]
 
     def clear(self):
         self.messages = []
-
